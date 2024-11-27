65fef26bab6d8ff72aef2a224fd476d03b333d43
gsmereka <gsmereka200@gmail.com>
2024-11-27 20:29:55 -0300

    add03

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -3,21 +3,21 @@ def NULL_not_found(object: any) -> int:
 
     if object != object:
         print(f"Cheese: nan <class '{type(object).__name__}'>") 
-        return 0  
+        return 1  
 
     for recognized_null in recognized_null_types:
         if object == recognized_null:
             if object is None:
                 name = "Nothing"
+            elif object == False and isinstance(object, bool):
+                name = "Fake"
             elif object == 0:
                 name = "Zero"
             elif object == '':
                 name = "Empty"
-            elif object == False:
-                name = "Fake"
             
-            print(f"{name} : {type(object).__name__}")
-            return 0  
+            print(f"{name} : {object} {type(object)}")
+            return 1  
 
     print("Type not Found")
     return 1  
