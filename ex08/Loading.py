1c169559c04a9d38d6f221af896277c3f7c7477e
gsmereka <gsmereka200@gmail.com>
2024-11-28 16:29:54 -0300

    att doc ex08

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -2,6 +2,23 @@ import sys
 
 
 def ft_tqdm(lst: range):
+    """
+    Displays a progress bar in the terminal while iterating over a sequence.
+
+    Acts as a generator that yields elements from the input sequence (`lst`)
+    and updates a progress bar in the terminal.
+
+    Args:
+        lst (range): The sequence to iterate over.
+
+    Yields:
+        The next element in the sequence.
+
+    Example:
+        for elem in ft_tqdm(range(100)):
+            # Perform some operation
+            pass
+    """
     total = len(lst)
 
     for i, elem in enumerate(lst):
